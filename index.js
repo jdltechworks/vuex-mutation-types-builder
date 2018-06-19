008b0da211ffc042a1ed38bcec8e1cea41eac4e2
Glen  Bangkila <hadeinvane@gmail.com>
2018-06-19 21:49:44 +0800

    Propose generate mutation type value

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-const createMutationTypes2 = (name) => {
-    return (...definitions) => (local) => {
-        return definitions.reduce((r, v, k) => {
-            r[v] = `${local ? '' : name + '/'}${v}`
-            return r;
-        }, {});
-    };
-};
+// const createMutationTypes = name => {
+//     return (...definitions) => {
+//         return definitions.reduce((r, v, k) => {
+//             r[v] = `${typeof name == 'undefined' ? '' : name + '/'}${v}`
+//             return r;
+//         }, {});
+//     };
+// };
 
+export function studlyCase(string) {
+    return string.replace(/[\-|\_]/g, ' ')
+                .replace(/\w\S*/g, str => {
+                    return str.charAt(0).toUpperCase() + str.substr(1).toLowerCase();
+                })
+                .replace(/\s/g, '')
+                .replace(/\w\S*/g, str => {
+                    return str.charAt(0).toLowerCase() + str.substr(1);
+                });
+}
 
-const createMutationTypes = (...definitions) => {
-    return (name) => {
-
-        console.log(name, definitions);
-        return definitions;
-    };
-};
+const createMutationTypes = name => (...definitions) => definitions.reduce((collection, key) => {
+    collection[key] = (typeof name == 'undefined' || name == null) ? studlyCase(key) : studlyCase(`${key} ${name}`);
+    return collection;
+}, {});
 
 export default createMutationTypes;
-
-
-export {
-    createMutationTypes,
-    createMutationTypes2
-};
--- index.test.js
+++ index.test.js
@@ -1,22 +1,38 @@
 import createMutationTypes from './index';
-
-test(`Build mutation types`, () => {
-    const types = createMutationTypes(
+it(`Build mutation types`, () => {
+    const types = createMutationTypes()(
         'FETCH',
         'STORE',
         'UPDATE',
         'DESTROY'
     );
     expect(types).toBeDefined();
+    expect(types.FETCH).toBe('fetch');
+    expect(types.fetch).toBeUndefined();
 });
 
 
-test(`Build mutation types with namespace`, () => {
-    const types = createMutationTypes(
+it(`Build mutation types with namespace`, () => {
+    const types = createMutationTypes('post')(
+        'FETCH',
+        'FETCH_ALL',
+        'STORE',
+        'UPDATE',
+        'DESTROY'
+    );
+    expect(types).toBeDefined();
+    expect(types.FETCH).toBe('fetchPost');
+    expect(types.FETCH_ALL).toBe('fetchAllPost');
+    expect(types.DESTROY_ALL).toBeUndefined();
+});
+
+it(`Build mutation types with destructuring`, () => {
+    let { FETCH, STORE } = createMutationTypes('post')(
         'FETCH',
         'STORE',
         'UPDATE',
         'DESTROY'
-    )('namespace');
-    expect(types).toBeDefined();
+    );
+    expect(FETCH).toBe('fetchPost');
+    expect(STORE).toBe('storePost');
 });
